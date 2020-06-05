e1b9027fdc77e392e3c60e256bcf1c683b5f284a
sevskii <sevskii111@gmail.com>
2020-06-05 11:00:12 +0300

    small fixes

--- server/settings.h
+++ server/settings.h
@@ -2,9 +2,9 @@
 
 #define MAP_SIZE 16
 #define MAX_PLAYERS 8
-#define BOTS 1 //max amount of bots that will be added after game start
+#define BOTS 8
 #define START_SNAKE_LEN 3
 #define FOOD_TARGET 4
 #define SCOREBOARD_TIME 5
-#define BOT_SPEED_UP 1 //bots can make decision when they are not active player
+#define BOT_SPEED_UP 1
 #define TICKRATE 24
